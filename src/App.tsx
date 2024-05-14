aaef15c362c93ae6815d0505c5ff86b42cfd6e72
andbudha <andreibartov@gmail.com>
2024-05-14 15:33:13 +0200

    Home comp created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import styles from './App.module.scss';
 import { Footer } from './components/Footer/Footer';
 import { Navbar } from './components/Navbar/Navbar';
+import { Home } from './components/pages/Home/Home';
 function App() {
   return (
     <div className={styles.app_main_box}>
       <div className={styles.app_box}>
         <Navbar />
+        <Home />
         <Footer />
       </div>
     </div>
---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import styles from './Home.module.scss';
+
+export const Home = () => {
+  return (
+    <div className={styles.home_main_box}>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
