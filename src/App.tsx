a8383220b48ae333371141bf666b92842871720c
andbudha <andreibartov@gmail.com>
2024-05-14 14:27:08 +0200

    Navbar comp created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import styles from './App.module.scss';
+import { Navbar } from './components/Navbar/Navbar';
 function App() {
   return (
     <div className={styles.app_main_box}>
-      <div className={styles.app_box}> </div>
+      <div className={styles.app_box}>
+        <Navbar />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,5 @@
+import styles from './Navbar.module.scss';
+
+export const Navbar = () => {
+  return <div className={styles.nav_main_box}></div>;
+};
