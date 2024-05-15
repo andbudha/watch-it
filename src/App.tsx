a2b6d35a31e1a154d45753a3d54076d48845ffe6
andbudha <andreibartov@gmail.com>
2024-05-15 14:21:19 +0200

    Data fetching moved into App comp

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,39 @@ import { Route, Routes } from 'react-router-dom';
 import styles from './App.module.scss';
 import { Layout } from './components/Layout/Layout';
 import { Home } from './components/pages/Home/Home';
-import { Movie } from './components/pages/MoviePage/Movie';
+import { MovieDetails } from './components/pages/MovieDetailsPage/MovieDetails';
 import { GridMovies } from './components/pages/Home/GridMovies/GridMovies';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Movies } from './assets/types/common_types';
 function App() {
+  const [movies, setMovies] = useState<null | Movies>(null);
+
+  console.log(movies);
+
+  const fetchMovies = async () => {
+    const response = await axios.get<Movies>(
+      'https://5b81e3264853b358.mokky.dev/mixedmovies'
+    );
+    if (response) {
+      setMovies(response.data.slice(0, 10));
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <div className={styles.app_main_box}>
       <div className={styles.app_box}>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+            <Route index element={<Home movies={movies} />} />
             <Route index path="movies" element={<GridMovies />} />
-            <Route path="movies/movie" element={<Movie />} />
+            <Route
+              path="movies/movie/:id"
+              element={<MovieDetails movies={movies} />}
+            />
           </Route>
         </Routes>
       </div>
