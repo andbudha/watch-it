497d8fefcdb6deebd5054bd2516a7ff29d7bd0f1
andbudha <andreibartov@gmail.com>
2024-05-15 12:00:24 +0200

    component local state used to set fetched data

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import styles from './Home.module.scss';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Movies } from '../../../assets/types/common_types';
+import { GridMovies } from './GridMovies/GridMovies';
 
 export const Home = () => {
+  const [movies, setMovies] = useState<null | Movies>(null);
+
+  console.log(movies);
+
   const fetchMovies = async () => {
-    const response = await axios.get(
+    const response = await axios.get<Movies>(
       'https://5b81e3264853b358.mokky.dev/mixedmovies'
     );
     if (response) {
-      console.log(response.data);
+      setMovies(response.data.slice(0, 8));
     }
   };
 
@@ -17,7 +23,7 @@ export const Home = () => {
   }, []);
   return (
     <div className={styles.home_main_box}>
-      <h2>Home Page</h2>
+      <GridMovies />{' '}
     </div>
   );
 };
