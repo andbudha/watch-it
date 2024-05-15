93c1764b8dc291d4e91f61237b753f29c7a8bdf9
andbudha <andreibartov@gmail.com>
2024-05-15 11:17:45 +0200

    Data fetched

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,6 +1,20 @@
+import axios from 'axios';
 import styles from './Home.module.scss';
+import { useEffect } from 'react';
 
 export const Home = () => {
+  const fetchMovies = async () => {
+    const response = await axios.get(
+      'https://5b81e3264853b358.mokky.dev/mixedmovies'
+    );
+    if (response) {
+      console.log(response.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <div className={styles.home_main_box}>
       <h2>Home Page</h2>
