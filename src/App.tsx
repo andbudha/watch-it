a38d370f7e506335c0e1c9c9ace2918999297f57
andbudha <andreibartov@gmail.com>
2024-05-15 12:01:31 +0200

    GridMovies comp added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from './App.module.scss';
 import { Layout } from './components/Layout/Layout';
 import { Home } from './components/pages/Home/Home';
 import { Movie } from './components/pages/MoviePage/Movie';
+import { GridMovies } from './components/pages/Home/GridMovies/GridMovies';
 function App() {
   return (
     <div className={styles.app_main_box}>
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route index path="movies" element={<GridMovies />} />
             <Route path="movies/movie" element={<Movie />} />
           </Route>
         </Routes>
---
+++ src/components/pages/Home/GridMovies/GridMovies.tsx
@@ -0,0 +1,9 @@
+import styles from './GridMovies.module.scss';
+
+export const GridMovies = () => {
+  return (
+    <div className={styles.movies_main_box}>
+      <h2>Movies</h2>
+    </div>
+  );
+};
