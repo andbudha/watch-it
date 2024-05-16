614e9443476a4bf5365ab29f1defb7612580ab57
andbudha <andreibartov@gmail.com>
2024-05-16 17:30:00 +0200

    Login page added

---
+++ src/components/pages/Login/Login.tsx
@@ -0,0 +1,14 @@
+import styles from './Login.module.scss';
+
+export const Login = () => {
+  return (
+    <div className={styles.login_main_box}>
+      <div className={styles.login_box}>
+        <h1>Login Page</h1>
+        <h1>Login Page</h1>
+        <h1>Login Page</h1>
+        <h1>Login Page</h1>
+      </div>
+    </div>
+  );
+};
