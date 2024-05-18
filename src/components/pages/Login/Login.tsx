1ae89df288caf16aa94b37b722d469d610fb079a
andbudha <andreibartov@gmail.com>
2024-05-18 09:13:01 +0200

    Login comp updated/getting form-values and handling errors func-ties added

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,13 +1,101 @@
+import { useFormik } from 'formik';
 import styles from './Login.module.scss';
+import { NavLink } from 'react-router-dom';
 
-export const Login = () => {
+type LoginProps = {};
+export const Login = ({}: LoginProps) => {
+  type LoginValuesType = {
+    email: string;
+    password: string;
+  };
+
+  type LoginErrorType = {
+    email: string;
+    password: string;
+  };
+  const validate = (values: LoginValuesType) => {
+    const errors: LoginErrorType = {
+      email: '',
+      password: '',
+    };
+    if (!values.email) {
+      errors.email = 'Email is required!';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+    if (!values.password) {
+      errors.password = 'Password is required!';
+    } else if (values.password.length < 6) {
+      errors.password = 'Must be at least 6 characters';
+    }
+    return errors;
+  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate,
+    onSubmit: (values: LoginValuesType) => {
+      console.log(values.email);
+      console.log(values.password);
+    },
+  });
   return (
     <div className={styles.login_main_box}>
       <div className={styles.login_box}>
-        <h1>Login Page</h1>
-        <h1>Login Page</h1>
-        <h1>Login Page</h1>
-        <h1>Login Page</h1>
+        <form className={styles.login_form} onSubmit={formik.handleSubmit}>
+          <div className={styles.label_box}>
+            <label htmlFor="email">Email Address</label>
+          </div>
+          <div className={styles.input_box}>
+            <input
+              className={styles.login_input}
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+            {formik.errors.email ? (
+              <div className={styles.formik_error_text}>
+                {formik.errors.email}
+              </div>
+            ) : null}
+          </div>
+          <div className={styles.label_box}>
+            <label htmlFor="password">Password</label>
+          </div>
+          <div className={styles.input_box}>
+            <input
+              className={styles.login_input}
+              id="password"
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+            {formik.errors.password ? (
+              <div className={styles.formik_error_text}>
+                {formik.errors.password}
+              </div>
+            ) : null}
+          </div>
+
+          <button className={styles.login_button} type="submit">
+            Login
+          </button>
+          <div className={styles.info_box}>
+            <span className={styles.info_text}>
+              Don't have an account?{' '}
+              <NavLink to={'/signup'} className={styles.signup_link}>
+                Sign Up
+              </NavLink>
+            </span>
+          </div>
+        </form>
       </div>
     </div>
   );
