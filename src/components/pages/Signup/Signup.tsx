53dd55ee9353683fc1af30d1a870dd1490afa8ca
andbudha <andreibartov@gmail.com>
2024-05-18 16:20:00 +0200

    Signup comp updated/getting form-values and handling errors func-ties implemented

--- src/components/pages/Signup/Signup.tsx
+++ src/components/pages/Signup/Signup.tsx
@@ -7,24 +7,77 @@ export const Signup = () => {
     useState<string>('');
   const [signupPasswordInputValue, setSignupPasswordInputValue] =
     useState<string>('');
+  const [signupEmailInputError, setSignupEmailInputError] =
+    useState<boolean>(false);
+  const [signupPasswordInputError, setSignupPasswordInputError] =
+    useState<boolean>(false);
+
+  type SignupValuesType = {
+    email: string;
+    password: string;
+  };
+  type SignupErrorsType = {
+    email: string;
+    password: string;
+  };
+  const signupValues: SignupValuesType = {
+    email: signupEmailInputValue,
+    password: signupPasswordInputValue,
+  };
 
   const catchSignupEmailValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setSignupEmailInputValue(e.currentTarget.value);
+    if (validation.email.length > 0) {
+      setSignupEmailInputError(true);
+    }
   };
 
   const catchSignupPasswordValueHandler = (
     e: ChangeEvent<HTMLInputElement>
   ) => {
     setSignupPasswordInputValue(e.currentTarget.value);
+    if (validation.password.length > 0) {
+      setSignupPasswordInputError(true);
+    }
   };
 
-  const submitSignupValuesHandler = () => {
-    const signupValues = {
-      email: signupEmailInputValue,
-      password: signupPasswordInputValue,
+  const validate = (values: SignupValuesType) => {
+    const errors: SignupErrorsType = {
+      email: '',
+      password: '',
     };
-    console.log(signupValues);
+    if (!values.email) {
+      errors.email = 'Email is required!';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+    if (!values.password) {
+      errors.password = 'Password is required!';
+    } else if (values.password.length < 6) {
+      errors.password = 'Must be at least 6 characters';
+    }
+    return errors;
   };
+
+  const validation = validate(signupValues);
+
+  console.log(validation);
+
+  const submitSignupValuesHandler = () => {
+    if (validation.email && validation.password) {
+      setSignupEmailInputError(true);
+      setSignupPasswordInputError(true);
+    } else if (validation.email) {
+      setSignupEmailInputError(true);
+    } else if (validation.password) {
+      setSignupPasswordInputError(true);
+    } else if (!validation.email && !validation.password) {
+      console.log(signupValues);
+    }
+  };
+
   return (
     <div className={styles.signup_main_box}>
       <div className={styles.signup_box}>
@@ -39,6 +92,9 @@ export const Signup = () => {
               value={signupEmailInputValue}
               type="email"
             />
+            {signupEmailInputError && validation.email && (
+              <span className={styles.error_text}>{validation.email}</span>
+            )}
           </div>
           <div className={styles.label_box}>
             <label htmlFor="password">Password</label>
@@ -50,6 +106,9 @@ export const Signup = () => {
               value={signupPasswordInputValue}
               type="password"
             />
+            {signupPasswordInputError && validation.password && (
+              <span className={styles.error_text}>{validation.password}</span>
+            )}
           </div>
           <div
             className={styles.signup_button}
