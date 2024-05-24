8ff84d01a15497178ca2976f726a8f41c2915993
andbudha <andreibartov@gmail.com>
2024-05-24 10:39:48 +0200

    Home comp updated/Search&Pagination comps added

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,13 +1,16 @@
 import styles from './Home.module.scss';
 import { GridMovies } from './GridMovies/GridMovies';
 import { Movie } from '../../../assets/types/common_types';
+import { Pagination } from '../../Pagination/Pagination';
+import { Search } from '../../Search/Search';
 type HomeProps = {
   movies: Movie[] | null;
 };
 export const Home = ({ movies }: HomeProps) => {
   return (
     <div className={styles.home_main_box}>
-      <GridMovies movies={movies} />{' '}
+      <Search />
+      <GridMovies movies={movies} /> <Pagination />
     </div>
   );
 };
