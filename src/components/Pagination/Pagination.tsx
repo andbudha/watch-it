3f829efc4f43fe33ab10412c3283b47bb87a2508
andbudha <andreibartov@gmail.com>
2024-05-24 10:31:01 +0200

    Pagination comp added

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,9 @@
+import styles from './Pagination.module.scss';
+
+export const Pagination = () => {
+  return (
+    <div className={styles.pagination_main_box}>
+      <div className={styles.pagination_box}></div>
+    </div>
+  );
+};
