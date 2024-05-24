265ee2872d3b410451f2cb195058639f98561307
andbudha <andreibartov@gmail.com>
2024-05-24 10:39:02 +0200

    Search comp created&styled

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,39 @@
+import { useState, ChangeEvent } from 'react';
+import { CiSearch } from 'react-icons/ci';
+import { RxCross2 } from 'react-icons/rx';
+import styles from './Search.module.scss';
+
+export const Search = () => {
+  const [searchInputValue, setSearchInputValue] = useState<string>('');
+
+  const getSearchInputValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchInputValue(event.currentTarget.value);
+  };
+  const emptySearchInputHandler = () => {
+    setSearchInputValue('');
+  };
+  return (
+    <div className={styles.search_main_box}>
+      <div className={styles.search_box}>
+        <div className={styles.search_icon_box}>
+          {' '}
+          <CiSearch className={styles.search_icon} />
+        </div>{' '}
+        <input
+          value={searchInputValue}
+          className={styles.search_input}
+          onChange={getSearchInputValueHandler}
+        />
+        <div className={styles.remove_icon_box}>
+          {' '}
+          {searchInputValue && (
+            <RxCross2
+              className={styles.remove_icon}
+              onClick={emptySearchInputHandler}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
