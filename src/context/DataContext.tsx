41c29c7cbf2b02989d7b1406502d1017521a9b54
andbudha <andreibartov@gmail.com>
2024-05-25 11:27:30 +0200

    users collection fetched from firestore/types for the newly fetched data added

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,12 +1,14 @@
 import { ReactNode, createContext, useState } from 'react';
 import { ListMovieType, Movies } from '../assets/types/common_types';
-import { DataBase } from '../config/firebase';
+import { auth, dataBase } from '../config/firebase';
 import {
-  addDoc,
+  DocumentReference,
+  arrayUnion,
   collection,
   deleteDoc,
   doc,
   getDocs,
+  updateDoc,
 } from 'firebase/firestore';
 import axios from 'axios';
 
@@ -16,8 +18,8 @@ type NewMovieToAddType = {
   userID?: string;
 };
 type DataContextType = {
+  usersCollection: UsersCollectionType[] | null;
   fireStoreMovieList: ListMovieType[] | null;
-  getMovieList: () => Promise<void>;
   getUsers: () => Promise<void>;
   movies: Movies | null;
   fetchMovies: () => Promise<void>;
@@ -25,11 +27,19 @@ type DataContextType = {
   deleteItemFromMyList: (movieID: string) => Promise<void>;
 };
 
+type CollectionUser = {
+  email: string;
+  id: string;
+  movieList: NewMovieToAddType[];
+};
+type UsersCollectionType = {
+  users: CollectionUser;
+};
 type DataProviderProps = { children: ReactNode };
 
 const initialDataContextState = {
+  usersCollection: [] as UsersCollectionType[],
   fireStoreMovieList: [] as ListMovieType[],
-  getMovieList: () => Promise.resolve(),
   getUsers: () => Promise.resolve(),
   movies: [] as Movies,
   fetchMovies: () => Promise.resolve(),
@@ -44,6 +54,8 @@ export const DataProvider = ({ children }: DataProviderProps) => {
   const [fireStoreMovieList, setFireStoreMovieList] = useState<
     null | ListMovieType[]
   >(null);
+  const [usersCollection, setUsersCollection] =
+    useState<UsersCollectionType | null>(null);
 
   // const localData = '../../data/movies.json';
   const fetchMovies = async () => {
@@ -55,20 +67,8 @@ export const DataProvider = ({ children }: DataProviderProps) => {
     }
   };
 
-  const selectedMovies = collection(DataBase, 'movie-list');
-  const getMovieList = async () => {
-    try {
-      const response = await getDocs(selectedMovies);
-      const data = response.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setFireStoreMovieList(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const usersList = collection(DataBase, 'users');
+  const usersList = collection(dataBase, 'users');
+
   const getUsers = async () => {
     try {
       const response = await getDocs(usersList);
@@ -77,25 +77,29 @@ export const DataProvider = ({ children }: DataProviderProps) => {
         id: doc.id,
       }));
       console.log(data);
-
+      setUsersCollection(data);
       setFireStoreMovieList(data);
     } catch (error) {
       console.log(error);
     }
   };
-  const movieListRef = collection(DataBase, 'users');
+
+  let movieListRef: DocumentReference<unknown, {}>;
+  if (auth.currentUser) {
+    movieListRef = doc(dataBase, 'users', auth.currentUser.uid);
+  }
+
   const addMovieToMyList = async (newMovie: NewMovieToAddType) => {
     try {
-      const response = await addDoc(
-        collection(movieListRef, 'movieList'),
-        newMovie
-      );
+      const response = await updateDoc(movieListRef, {
+        movieList: arrayUnion(newMovie),
+      });
       console.log(response);
     } catch (error) {}
   };
 
   const deleteItemFromMyList = async (movieID: string) => {
-    const docToBeRemoved = doc(DataBase, 'movie-list', movieID);
+    const docToBeRemoved = doc(dataBase, 'movie-list', movieID);
     try {
       await deleteDoc(docToBeRemoved);
     } catch (error) {
@@ -105,11 +109,11 @@ export const DataProvider = ({ children }: DataProviderProps) => {
   return (
     <DataContext.Provider
       value={{
+        usersCollection,
         getUsers,
         fetchMovies,
         movies,
         fireStoreMovieList,
-        getMovieList,
         addMovieToMyList,
         deleteItemFromMyList,
       }}
