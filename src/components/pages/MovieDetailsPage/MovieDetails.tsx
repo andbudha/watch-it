91e38b7a2a83e8f257c4ac3cace6a064a431e924
andbudha <andreibartov@gmail.com>
2024-05-25 19:17:46 +0200

    home button added in MovieDetails comp

--- src/components/pages/MovieDetailsPage/MovieDetails.tsx
+++ src/components/pages/MovieDetailsPage/MovieDetails.tsx
@@ -1,9 +1,10 @@
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import styles from './MovieDetails.module.scss';
 import { Movie } from '../../../assets/types/common_types';
 import { useContext } from 'react';
 import { AuthContext } from '../../../context/AuthContext';
 import { DataContext } from '../../../context/DataContext';
+import { IoChevronBack } from 'react-icons/io5';
 
 type MovieProps = {
   movies: Movie[] | null;
@@ -24,61 +25,69 @@ export const MovieDetails = ({ movies }: MovieProps) => {
       thumbnail: movie?.thumbnail,
       id: movie?.title,
     };
-    console.log(movieToAdd);
     addMovieToMyList(movieToAdd);
     getUsers();
   };
 
   return (
     <div className={styles.movie_details_main_box}>
-      <div className={styles.img_and_button_box}>
-        <div className={styles.movie_img_box}>
-          <img
-            className={styles.movie_img}
-            src={movie?.thumbnail}
-            alt={`movie poster`}
-          />
+      <div className={styles.movie_details_box}>
+        {' '}
+        <div className={styles.img_and_button_box}>
+          <div className={styles.movie_img_box}>
+            <img
+              className={styles.movie_img}
+              src={movie?.thumbnail}
+              alt={`movie poster`}
+            />
+          </div>
+          <div
+            className={styles.add_movie_button}
+            onClick={addMovieToMyListHandler}
+          >
+            add to my list
+          </div>
+          <NavLink className={styles.home_button} to={'/'}>
+            {' '}
+            <IoChevronBack className={styles.chevron_icon} />
+            main
+          </NavLink>
         </div>
-        <div
-          className={styles.add_movie_button}
-          onClick={addMovieToMyListHandler}
-        >
-          add to my list
+        <div className={styles.movie_info_box}>
+          <div className={styles.title_box}>
+            <h3 className={styles.movie_details_header}>
+              Title:{' '}
+              <span className={styles.movie_details}>{movie?.title}</span>
+            </h3>
+          </div>
+          <div className={styles.genre_box}>
+            <h3 className={styles.movie_details_header}>
+              Genre: <span className={styles.movie_details}>{genreList}</span>
+            </h3>
+          </div>
+          <div className={styles.release_year_box}>
+            <h3 className={styles.movie_details_header}>
+              Release Year:{' '}
+              <span className={styles.movie_details}>{movie?.year}</span>
+            </h3>
+          </div>
+          <div className={styles.cast_box}>
+            <h3 className={styles.movie_details_header}>
+              Cast:{' '}
+              <span className={styles.movie_details}>
+                {castList?.length ? castList : 'Unknown'}
+              </span>
+            </h3>
+          </div>
+          <div className={styles.story_box}>
+            <h3 className={styles.movie_details_header}>
+              Story:{' '}
+              <span className={styles.movie_details}>{movie?.extract}</span>
+            </h3>
+          </div>
         </div>
       </div>
-
-      <div className={styles.movie_detail_box}>
-        <div className={styles.title_box}>
-          <h3 className={styles.movie_details_header}>
-            Title: <span className={styles.movie_details}>{movie?.title}</span>
-          </h3>
-        </div>
-        <div className={styles.genre_box}>
-          <h3 className={styles.movie_details_header}>
-            Genre: <span className={styles.movie_details}>{genreList}</span>
-          </h3>
-        </div>
-        <div className={styles.release_year_box}>
-          <h3 className={styles.movie_details_header}>
-            Release Year:{' '}
-            <span className={styles.movie_details}>{movie?.year}</span>
-          </h3>
-        </div>
-        <div className={styles.cast_box}>
-          <h3 className={styles.movie_details_header}>
-            Cast:{' '}
-            <span className={styles.movie_details}>
-              {castList?.length ? castList : 'Unknown'}
-            </span>
-          </h3>
-        </div>
-        <div className={styles.story_box}>
-          <h3 className={styles.movie_details_header}>
-            Story:{' '}
-            <span className={styles.movie_details}>{movie?.extract}</span>
-          </h3>
-        </div>
-      </div>
+      <div className={styles.home_button_box}> </div>
     </div>
   );
 };
