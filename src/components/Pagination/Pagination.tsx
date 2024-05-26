8e356126a5e91dd20ff6e5709ab5b1c99c42dc8b
andbudha <andreibartov@gmail.com>
2024-05-26 15:58:51 +0200

    Pagination comp renamed into Paginator

--- src/components/Pagination/Pagination.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Pagination.module.scss';
-
-export const Pagination = () => {
-  return (
-    <div className={styles.pagination_main_box}>
-      <div className={styles.pagination_box}></div>
-    </div>
-  );
-};
