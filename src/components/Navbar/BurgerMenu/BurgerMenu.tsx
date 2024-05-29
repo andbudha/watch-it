5d65ede4b0439f1cd7c2eb4094955a5dfccd0f85
andbudha <andreibartov@gmail.com>
2024-05-29 14:43:00 +0200

    BurgerMenu comp updated

--- src/components/Navbar/BurgerMenu/BurgerMenu.tsx
+++ src/components/Navbar/BurgerMenu/BurgerMenu.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import styles from './BurgerMenu.module.scss';
 import { RxHamburgerMenu, RxCross2 } from 'react-icons/rx';
+import { CiHome, CiLogin, CiLogout, CiUser, CiViewList } from 'react-icons/ci';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthContext';
+import { DataContext } from '../../../context/DataContext';
 
 export const BurgerMenu = () => {
+  const { logOutUser, user } = useContext(AuthContext);
+  const { getUsers, usersCollection } = useContext(DataContext);
   const [display, setDisplay] = useState<boolean>(false);
 
+  const currentUserList = usersCollection?.find(
+    (collectionUser) => collectionUser.id === user?.userID
+  )?.movieList;
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const logOutHandler = () => {
+    logOutUser();
+    setDisplay(!display);
+  };
   const toggleDisplayHandler = () => {
     setDisplay(!display);
   };
@@ -16,7 +32,76 @@ export const BurgerMenu = () => {
             ? styles.dropped_burger_menu_box
             : styles.hidden_burger_menu_box
         }
-      ></div>
+      >
+        {!user ? (
+          <div className={styles.auth_box}>
+            <NavLink
+              to={'/'}
+              className={styles.burger_menu_button_box}
+              // className={`${styles.burger_menu_button_box} ${(isAtive: boolean) =>
+              //   isAtive && styles.active}`}
+              onClick={toggleDisplayHandler}
+            >
+              <span className={styles.link_text}>main</span>
+              <CiHome className={styles.burger_menu_icon} />
+            </NavLink>
+            <NavLink
+              to={'login'}
+              className={styles.burger_menu_button_box}
+              onClick={toggleDisplayHandler}
+            >
+              <span className={styles.link_text}>login</span>
+              <CiLogin className={styles.burger_menu_icon} />
+            </NavLink>
+            <NavLink
+              to={'signup'}
+              className={styles.burger_menu_button_box}
+              onClick={toggleDisplayHandler}
+            >
+              <span className={styles.link_text}>signup</span>
+              <CiUser className={styles.burger_menu_icon} />
+            </NavLink>
+          </div>
+        ) : (
+          <div className={styles.nav_box}>
+            <NavLink
+              to={'/'}
+              className={styles.burger_menu_button_box}
+              // className={`${styles.burger_menu_button_box} ${(isAtive: boolean) =>
+              //   isAtive && styles.active}`}
+              onClick={toggleDisplayHandler}
+            >
+              <span className={styles.link_text}>main</span>
+              <CiHome className={styles.burger_menu_icon} />
+            </NavLink>
+            <NavLink
+              to={'mylist'}
+              className={styles.burger_menu_button_box}
+              onClick={toggleDisplayHandler}
+            >
+              {' '}
+              <span className={styles.link_text}>my list</span>
+              {currentUserList?.length ? (
+                <div className={styles.number_of_items_in_mylist_box}>
+                  {' '}
+                  <span className={styles.my_list_length}>
+                    {currentUserList.length}
+                  </span>
+                </div>
+              ) : (
+                <CiViewList className={styles.burger_menu_icon} />
+              )}
+            </NavLink>
+            <div
+              className={styles.burger_menu_button_box}
+              onClick={logOutHandler}
+            >
+              <span className={styles.link_text}>logout</span>
+              <CiLogout className={styles.burger_menu_icon} />
+            </div>
+          </div>
+        )}
+      </div>
       <div className={styles.burger_button_box}>
         {display ? (
           <RxCross2
