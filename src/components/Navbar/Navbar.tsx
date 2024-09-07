7554ff40bebf858a20668eadbebbebe3b90329cf
andbudha <andreibartov@gmail.com>
2024-09-07 21:57:35 +0200

    loggedin user greeting and email displayed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -22,75 +22,82 @@ export const Navbar = () => {
   };
   return (
     <div className={styles.nav_main_box}>
-      <NavLink to={'/'} className={styles.home_page_link_box}>
-        <RiMovie2Line className={styles.logo_icon} />
-        <div className={styles.logo_text_box}>
-          {' '}
-          <span className={styles.logo_text}>.watch..it</span>
-        </div>
-      </NavLink>
-      <div className={styles.links_box}>
-        {!user ? (
-          <div className={styles.auth_box}>
-            <NavLink
-              to={'login'}
-              className={styles.login_button_box}
-              style={({ isActive }) => ({
-                background: isActive ? '#864af9' : '',
-                color: isActive ? '#fff' : '',
-                border: isActive ? '2px solid #864af9' : '',
-              })}
-            >
-              <span className={styles.link_text}>login</span>
-              <CiLogin className={styles.login_icon} />
-            </NavLink>
-            <NavLink
-              to={'signup'}
-              className={styles.signin_button_box}
-              style={({ isActive }) => ({
-                background: isActive ? '#864af9' : '',
-                color: isActive ? '#fff' : '',
-                border: isActive ? '2px solid #864af9' : '',
-              })}
-            >
-              <span className={styles.link_text}>signup</span>
-              <CiUser className={styles.signin_icon} />
-            </NavLink>
+      <div className={styles.links_main_box}>
+        <NavLink to={'/'} className={styles.home_page_link_box}>
+          <RiMovie2Line className={styles.logo_icon} />
+          <div className={styles.logo_text_box}>
+            {' '}
+            <span className={styles.logo_text}>.watch..it</span>
           </div>
-        ) : (
-          <div className={styles.nav_box}>
-            <NavLink
-              to={'mylist'}
-              className={styles.my_list_link_main_box}
-              style={({ isActive }) => ({
-                background: isActive ? '#864af9' : '',
-                color: isActive ? '#fff' : '',
-                border: isActive ? '2px solid #864af9' : '',
-              })}
-            >
-              {user && (
-                <div className={styles.my_list_link_box}>
-                  {' '}
-                  <span className={styles.link_text}>my list</span>
-                  {currentUserList?.length ? (
-                    <div className={styles.number_of_items_in_mylist_box}>
-                      {' '}
-                      <span className={styles.my_list_length}>
-                        {currentUserList.length}
-                      </span>
-                    </div>
-                  ) : (
-                    <CiViewList className={styles.list_icon} />
-                  )}
-                </div>
-              )}
-            </NavLink>
-            <div className={styles.logout_button_box} onClick={logOutHandler}>
-              <span className={styles.link_text}>logout</span>
-              <CiLogout className={styles.logout_icon} />
+        </NavLink>
+        <div className={styles.links_box}>
+          {!user ? (
+            <div className={styles.auth_box}>
+              <NavLink
+                to={'login'}
+                className={styles.login_button_box}
+                style={({ isActive }) => ({
+                  background: isActive ? '#864af9' : '',
+                  color: isActive ? '#fff' : '',
+                  border: isActive ? '2px solid #864af9' : '',
+                })}
+              >
+                <span className={styles.link_text}>login</span>
+                <CiLogin className={styles.login_icon} />
+              </NavLink>
+              <NavLink
+                to={'signup'}
+                className={styles.signin_button_box}
+                style={({ isActive }) => ({
+                  background: isActive ? '#864af9' : '',
+                  color: isActive ? '#fff' : '',
+                  border: isActive ? '2px solid #864af9' : '',
+                })}
+              >
+                <span className={styles.link_text}>signup</span>
+                <CiUser className={styles.signin_icon} />
+              </NavLink>
             </div>
-          </div>
-        )}
+          ) : (
+            <div className={styles.nav_box}>
+              <NavLink
+                to={'mylist'}
+                className={styles.my_list_link_main_box}
+                style={({ isActive }) => ({
+                  background: isActive ? '#864af9' : '',
+                  color: isActive ? '#fff' : '',
+                  border: isActive ? '2px solid #864af9' : '',
+                })}
+              >
+                {user && (
+                  <div className={styles.my_list_link_box}>
+                    {' '}
+                    <span className={styles.link_text}>my list</span>
+                    {currentUserList?.length ? (
+                      <div className={styles.number_of_items_in_mylist_box}>
+                        {' '}
+                        <span className={styles.my_list_length}>
+                          {currentUserList.length}
+                        </span>
+                      </div>
+                    ) : (
+                      <CiViewList className={styles.list_icon} />
+                    )}
+                  </div>
+                )}
+              </NavLink>
+              <div className={styles.logout_button_box} onClick={logOutHandler}>
+                <span className={styles.link_text}>logout</span>
+                <CiLogout className={styles.logout_icon} />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className={styles.greeting_main_box}>
+        <h4 className={styles.greeting}>
+          Hello, <span className={styles.user_email}>{user?.email}</span>
+        </h4>
       </div>
     </div>
   );
